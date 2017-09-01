37344f56d482574dd668b7a2d897141ee94e818d
leiradel <andre@leiradella.com>
2017-09-01 22:31:29 +0100

    Remove unnecessary #undefs

--- src/imguiext/imguidock.cpp
+++ src/imguiext/imguidock.cpp
@@ -27,9 +27,7 @@ SOFTWARE.
 
 //- Common Code For All Addons needed just to ease inclusion as separate files in user code ----------------------
 #include <imgui.h>
-#undef IMGUI_DEFINE_PLACEMENT_NEW
 #define IMGUI_DEFINE_PLACEMENT_NEW
-#undef IMGUI_DEFINE_MATH_OPERATORS
 #define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui_internal.h>
 //-----------------------------------------------------------------------------------------------------------------
